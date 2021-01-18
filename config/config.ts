c53ec7d6a8ae0de3a9ec31d6eb906c11a1ac7c61
diakitelamine <42340818+diakitelamine@users.noreply.github.com>
2021-01-18 13:48:35 +0100

    First commit

--- config/config.ts
+++
@@ -1,2 +0,0 @@
-import { config as loadConfig } from "https://deno.land/x/dotenv@v1.0.1/mod.ts"
-export const config = loadConfig();
--- types/roleTypes.ts
+++
@@ -1,1 +0,0 @@
-export type roleTypes = "Admin" | "User" | "Modo";
